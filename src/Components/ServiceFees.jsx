4a5d7803e760c3d5b1502a4ef7965b7fb1d571f3
Andrii <andrewmalanchuk@gmail.com>
2019-12-07 16:06:30 -0800

    Add more functionality for the total price componenet

---
+++ src/Components/ServiceFees.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Wrapper, Container} from './styles/ServiceFees.js'
+
+let ServiceFees = props => {
+    return(
+        <Wrapper>
+           <Container>This helps us run our platform and offer services like 24/7 support on your trip.</Container>
+        </Wrapper>
+    )
+}
+
+export default ServiceFees;
